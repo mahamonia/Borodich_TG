a292f62f6c2428a91b06dc9d197ac86b1f32f7d0
Borodich Tania <mahamonia@mail.ru>
2017-08-18 11:51:35 +0300

    add new chain scan background

--- create_shema.sql
+++ create_shema.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS slb AUTHORIZATION slb;
 
-DROP TABLE IF EXISTS detail;
+DROP TABLE IF EXISTS detail CASCADE;
 
 CREATE TABLE slb.detail (
 		id serial NOT NULL UNIQUE PRIMARY KEY,
@@ -20,7 +20,7 @@ INSERT INTO detail (id,	name_ru, name_en, weight_average, weight_max, weight_min
 	VALUES (1, 'деталь #1', 'detail #1', 10, 20, 5, '111', '11_01', '', 'category_1');
 INSERT INTO detail (id,	name_ru, name_en, weight_average, weight_max, weight_min,
 					nomenclature_number, part_number, description, category)
-	VALUES (2, 'деталь #2', 'detail #2', 70, 200, 40, '222', '1721_01', '', 'category_1');
+	VALUES (2, 'деталь #2', 'detail #2', 70, 200, 11, '222', '1721_01', '', 'category_1');
 INSERT INTO detail (id,	name_ru, name_en, weight_average, weight_max, weight_min,
 					nomenclature_number, part_number, description, category)
 	VALUES (3, 'деталь #3', 'detail #3', 12, 70,18, '333', '13_01', '', 'category_3');
@@ -36,6 +36,12 @@ INSERT INTO detail (id,	name_ru, name_en, weight_average, weight_max, weight_min
 INSERT INTO detail (id,	name_ru, name_en, weight_average, weight_max, weight_min,
 					nomenclature_number, part_number, description, category)
 	VALUES (7, 'деталь #7', 'detail #7', 47, 57, 15, '777', '17_01', '', 'category_7');
+INSERT INTO detail (id,	name_ru, name_en, weight_average, weight_max, weight_min,
+					nomenclature_number, part_number, description, category)
+	VALUES (8, 'деталь #7', 'detail #7', 47, 57, 10, '777', '17_01', '', 'category_3');
+INSERT INTO detail (id,	name_ru, name_en, weight_average, weight_max, weight_min,
+					nomenclature_number, part_number, description, category)
+	VALUES (9, 'деталь #7', 'detail #7', 47, 57, 15, '777', '17_01', '', 'category_7');
 
 DROP TABLE IF EXISTS history;
 
@@ -48,4 +54,14 @@ CREATE TABLE slb.history (
 
 INSERT INTO history (id, weight, nomenclature_number, detail_fk)
 	VALUES (1, 75, '111', 1);
+
+DROP TABLE IF EXISTS image;
+
+CREATE TABLE slb.image (
+		id serial NOT NULL UNIQUE PRIMARY KEY,
+		image bytea NOT NULL,
+		point json NOT NULL,
+		description varchar(50) NOT NULL,
+		detail_fk serial REFERENCES detail
+		);
 
